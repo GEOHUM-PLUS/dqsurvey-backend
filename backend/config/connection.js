0363142a08be2076f46294dcfd86d63fb8bd3d75
MariaMushtaq-12 <maria.m.hashmi333@gmail.com>
2026-02-02 11:31:52 +0100

    section1

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,27 +1,4 @@
-// require('dotenv').config();
-// const mysql = require('mysql2/promise');
 
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const dbConfig = {
-//   host: isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
-//   user: isProduction ? process.env.DB_USER_PROD : process.env.DB_USER_DEV,
-//   password: isProduction ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV,
-//   database: isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME_DEV,
-//   port: isProduction ? process.env.DB_PORT_PROD : process.env.DB_PORT_DEV,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 30000 // 30 seconds
-// };
-
-// const pool = mysql.createPool(dbConfig);
-
-// pool.getConnection()
-//   .then(() => console.log(`✅ Connected to MySQL Database: ${dbConfig.database}`))
-//   .catch(err => console.error('❌ Database connection failed:', err.message));
-
-// module.exports = pool;
 
 // postgresql version below (not used currently)
 require('dotenv').config();
@@ -41,6 +18,21 @@ const dbConfig = {
 };
 
 const pool = new Pool(dbConfig);
+// const pool = new Pool({
+//   host: isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
+//   user: isProduction ? process.env.DB_USER_PROD : process.env.DB_USER_DEV,
+//   password: isProduction ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV,
+//   database: isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME_DEV,
+//   port: isProduction ? process.env.DB_PORT_PROD : process.env.DB_PORT_DEV,
+
+//   ssl: {
+//     rejectUnauthorized: false
+//   },
+
+//   max: 10,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 30000
+// });
 
 // Test connection
 pool.connect()
