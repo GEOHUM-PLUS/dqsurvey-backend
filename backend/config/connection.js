728f7cd84a5713499244502c29b3d812966c76e8
MariaMushtaq-12 <maria.m.hashmi333@gmail.com>
2026-02-10 13:20:48 +0100

    deployemnt code2

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -18,20 +18,28 @@ const dbConfig = {
 };
 
 
+// const pool = new Pool({
+//   host: isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
+//   user: isProduction ? process.env.DB_USER_PROD : process.env.DB_USER_DEV,
+//   password: isProduction ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV,
+//   database: isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME_DEV,
+//   port: isProduction ? process.env.DB_PORT_PROD : process.env.DB_PORT_DEV,
+
+//   ssl: {
+//     rejectUnauthorized: false
+//   },
+
+//   max: 10,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 30000
+// });
 const pool = new Pool({
-  host: isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
-  user: isProduction ? process.env.DB_USER_PROD : process.env.DB_USER_DEV,
-  password: isProduction ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV,
-  database: isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME_DEV,
-  port: isProduction ? process.env.DB_PORT_PROD : process.env.DB_PORT_DEV,
-
-  ssl: {
-    rejectUnauthorized: false
-  },
-
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 30000
+  host: process.env.DB_HOST_DEV,
+  port: process.env.DB_PORT_DEV,
+  user: process.env.DB_USER_DEV,
+  password: process.env.DB_PASSWORD_DEV,
+  database: process.env.DB_NAME_DEV,
+  ssl: { rejectUnauthorized: false }
 });
 
 // local connection
